3bd23aea05276c641320dd25badc5d391af727e2
Jonas Scharpf <jonas@brainelectronics.de>
2023-05-01 20:05:15 +0200

    update setup and package files

--- setup.py
+++ setup.py
@@ -11,15 +11,15 @@ here = Path(__file__).parent.resolve()
 long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 # load elements of version.py
-exec(open(here / 'be_upy_blink' / 'version.py').read())
+exec(open(here / 'ds1307' / 'version.py').read())
 
 setup(
-    name='micropython-package-template',
+    name='micropython-ds1307',
     version=__version__,
-    description="MicroPython PyPi package template project with auto deploy",
+    description=" MicroPython driver for DS1307 RTC ",
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/brainelectronics/micropython-package-template',
+    url='https://github.com/brainelectronics/micropython-ds1307',
     author='brainelectronics',
     author_email='info@brainelectronics.de',
     classifiers=[
@@ -28,13 +28,13 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: Implementation :: MicroPython',
     ],
-    keywords='micropython, template',
+    keywords='micropython, i2c, ds1307, driver',
     project_urls={
-        'Bug Reports': 'https://github.com/brainelectronics/micropython-package-template/issues',
-        'Source': 'https://github.com/brainelectronics/micropython-package-template',
+        'Bug Reports': 'https://github.com/brainelectronics/micropython-ds1307/issues',
+        'Source': 'https://github.com/brainelectronics/micropython-ds1307',
     },
     license='MIT',
     cmdclass={'sdist': sdist_upip.sdist},
-    packages=['be_upy_blink'],
+    packages=['ds1307'],
     install_requires=[]
 )
